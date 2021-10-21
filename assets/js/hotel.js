e8255c0e55885ad9ad3e474e73917dfb8073ca81
maxh1231 <maxhumpherys@gmail.com>
2021-10-20 20:13:38 -0600

    uncomment

--- assets/js/hotel.js
+++ assets/js/hotel.js
@@ -5,60 +5,60 @@ var location = new URLSearchParams(window.location.search).get("location");
 
 
 // hotel data fetch
-// let hotelDataCall = function () {
+let hotelDataCall = function () {
 
-//     fetch(`https://hotels4.p.rapidapi.com/locations/search?query=${apiQuery}&locale=en_US`, {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "hotels4.p.rapidapi.com",
-//             "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(function (hotelData) {
-//             console.log(hotelData);
-//             // for (let i = 0; i < 10; i++) {
-//             // 	console.log(data.suggestions[1].entities[i]);
+    fetch(`https://hotels4.p.rapidapi.com/locations/search?query=${apiQuery}&locale=en_US`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "hotels4.p.rapidapi.com",
+            "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
+        }
+    })
+        .then(response => response.json())
+        .then(function (hotelData) {
+            console.log(hotelData);
+            // for (let i = 0; i < 10; i++) {
+            // 	console.log(data.suggestions[1].entities[i]);
 
-//             // }
-
-
-
-
-//         })
-
-//         .catch(err => {
-//             console.error(err);
-//         });
+            // }
 
 
 
-// }
+
+        })
+
+        .catch(err => {
+            console.error(err);
+        });
+
+
+
+}
 
 // weather data fetch
-// let weatherDataCall = function () {
+let weatherDataCall = function () {
 
 
-//     fetch(`https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=${apiQuery}&contentType=json&unitGroup=us&shortColumnNames=0`, {
-//         "method": "GET",
-//         "headers": {
-//             "x-rapidapi-host": "visual-crossing-weather.p.rapidapi.com",
-//             "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
-//         }
-//     })
-//         .then(response => response.json())
-//         .then(function (weatherData) {
-//             console.log(weatherData);
+    fetch(`https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=${apiQuery}&contentType=json&unitGroup=us&shortColumnNames=0`, {
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "visual-crossing-weather.p.rapidapi.com",
+            "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
+        }
+    })
+        .then(response => response.json())
+        .then(function (weatherData) {
+            console.log(weatherData);
 
 
 
 
-//         })
-//         .catch(err => {
-//             console.error(err);
-//         });
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
-// }
+}
 
 window.onload = function () {
     weatherDataCall();
