f250356eafdd48f88aacb935b29df071b7a3dda8
maxh1231 <maxhumpherys@gmail.com>
2021-10-20 23:14:34 -0600

    merging develop with my branch

--- assets/js/hotel.js
+++ assets/js/hotel.js
@@ -1,13 +1,15 @@
+// geocode api key 16504850b1a264a95e1797ff5a4e056b
 
 var apiQuery = localStorage.getItem(localStorage.key(0));
 var userInput = new URLSearchParams(window.location.search).get("location");
 
 
 
+
 // hotel data fetch
 let hotelDataCall = function () {
 
-    fetch(`https://hotels4.p.rapidapi.com/locations/search?query=${location}&locale=en_US`, {
+    fetch(`https://hotels4.p.rapidapi.com/locations/search?query=${userInput}&locale=en_US`, {
         "method": "GET",
         "headers": {
             "x-rapidapi-host": "hotels4.p.rapidapi.com",
@@ -39,28 +41,42 @@ let hotelDataCall = function () {
 let weatherDataCall = function () {
 
 
-    fetch(`https://visual-crossing-weather.p.rapidapi.com/forecast?aggregateHours=24&location=${location}&contentType=json&unitGroup=us&shortColumnNames=0`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "visual-crossing-weather.p.rapidapi.com",
-            "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
-        }
-    })
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${userInput}&limit=5&appid=16504850b1a264a95e1797ff5a4e056b`)
+
+
         .then(response => response.json())
-        .then(function (weatherData) {
-            console.log(weatherData);
+        .then(function (geoData) {
+            console.log(geoData);
+            var lat = geoData[0].lat;
+            var lon = geoData[0].lon;
+
+
+            fetch(`https://aerisweather1.p.rapidapi.com/forecasts/${lat},${lon}`, {
+                "method": "GET",
+                "headers": {
+                    "x-rapidapi-host": "aerisweather1.p.rapidapi.com",
+                    "x-rapidapi-key": "26aa5aaa64msh7c71403c8404b50p1f70d8jsn250c3dfeb42e"
+                }
+            })
+                .then(response => response.json())
+                .then(function (weatherData) {
+                    console.log(weatherData);
+                    document.querySelector(".WR-date").textContent = weatherData.response[0].periods[0].dateTimeISO
+                    document.querySelector(".WR-temperature").textContent = "High: " + weatherData.response[0].periods[0].maxTempF + " Low: " + weatherData.response[0].periods[0].minTempF
+                    document.querySelector(".WR-wind").textContent = weatherData.response[0].periods[0].windSpeedMPH + " MPH"
+                    document.querySelector(".WR-humidity").textContent = weatherData.response[0].periods[0].humidity + " %"
+                    document.querySelector(".WR-uv-index").textContent = weatherData.response[0].periods[0].uvi
 
 
 
-
+                })
+                .catch(err => {
+                    console.error(err);
+                });
         })
-        .catch(err => {
-            console.error(err);
-        });
-
 }
 
-// window.onload = function () {
-//     weatherDataCall();
-//     hotelDataCall();
-// }
+window.onload = function () {
+    weatherDataCall();
+    // hotelDataCall();
+}
