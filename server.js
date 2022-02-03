5bb85046a7cde041d791d585938327da0f9feada
maxh1231 <maxhumpherys@gmail.com>
2022-02-02 23:18:36 -0700

    tailwind integration

--- server.js
+++ server.js
@@ -8,6 +8,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(require('./controllers'))
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+app.get('/hotels', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/hotel.html'));
+})
 
 app.listen(PORT, () => console.log(`now listening on ${PORT}`))
