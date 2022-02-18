8bd8f6e5760637a0beea4b4c48c86fe44557b152
maxh1231 <maxhumpherys@gmail.com>
2022-02-17 23:51:50 -0700

    removed images

--- public/js/hotel.js
+++ public/js/hotel.js
@@ -66,11 +66,10 @@ let hotelDataCall = function () {
                                 if (hotelPropertiesData.data.body.searchResults.results[i].address.streetAddress === undefined) {
                                     count++;
                                 } else {
-                                    document.querySelector(".HR-0".concat(i + 1 - count) + "-picture").src = hotelPropertiesData.data.body.searchResults.results[i].optimizedThumbUrls.srpDesktop
+
                                     document.querySelector(".HR-0".concat(i + 1 - count) + "-price").textContent = hotelPropertiesData.data.body.searchResults.results[i].ratePlan.price.current
                                     document.querySelector(".HR-0".concat(i + 1 - count) + "-rating").textContent = hotelPropertiesData.data.body.searchResults.results[i].starRating + " star"
-                                    document.querySelector(".HR-0".concat(i + 1 - count) + "-address").textContent = hotelPropertiesData.data.body.searchResults.results[i].address.streetAddress + ", "
-                                        + hotelPropertiesData.data.body.searchResults.results[i].address.locality + ", " + hotelPropertiesData.data.body.searchResults.results[i].address.region + " " + hotelPropertiesData.data.body.searchResults.results[i].address.postalCode
+
                                     document.querySelector(".HR-0".concat(i + 1 - count) + "-name").textContent = hotelPropertiesData.data.body.searchResults.results[i].name
                                     var hotelLocation = hotelPropertiesData.data.body.searchResults.results[i].coordinate
                                     document.querySelector(".HR-0".concat(i + 1 - count) + "-map").src = `https://www.google.com/maps/embed/v1/streetview?location=${hotelLocation["lat"]},${hotelLocation["lon"]}&key=AIzaSyAD1j26SQoCLmAFKABhY_QKa25HtuYWdhU`
