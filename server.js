832a71d5b645adbb42566139a9223eda786ed35b
maxh1231 <maxhumpherys@gmail.com>
2022-03-01 13:14:23 -0700

    heroku port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
